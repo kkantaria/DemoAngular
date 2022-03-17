3b06248eee514e56c584729493f2d19ddcb6872e
“sudeepyadav” <sudeep.yadav@hiddenbrains.in>
2022-03-17 17:42:50 +0530

    Map Page -- Pagination & Filter HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { HeaderComponent } from './header/header.component';
 import { MapComponent } from './map/map.component';
+import { FilterComponent } from './filter/filter.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MapComponent
+    MapComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     MatFormFieldModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSlideToggleModule
   ],
   entryComponents: [],
   providers: [],
